fa8ea907a865846f56b43516e6c5d8c6924a871b
cice <cice@aula8PovMacN08.local>
2017-02-17 17:33:49 +0100

    Primer commit de la app de introduccion de un CollectionView

--- App_IntroCollectionVC_SecondRound/ViewController.swift
+++ App_IntroCollectionVC_SecondRound/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var myCollectionViewCustom: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        myCollectionViewCustom.delegate = self
+        myCollectionViewCustom.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +29,24 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 500
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = myCollectionViewCustom.dequeueReusableCell(withReuseIdentifier: "celda", for: indexPath)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("De nuevo Hola Mundo")
+    }
+
+}
